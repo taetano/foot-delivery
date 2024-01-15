310aee83c8cac9e2f94477c42bff9af6527890a0
taetano <taetano96@gmail.com>
2024-01-15 13:42:10 +0900

    [Test] UserService 실패 케이스 추가

--- src/test/java/com/project/delivery/service/UserServiceTest.java
+++ src/test/java/com/project/delivery/service/UserServiceTest.java
@@ -3,6 +3,8 @@ package com.project.delivery.service;
 import com.project.delivery.dto.MyProfileResponseDto;
 import com.project.delivery.dto.SignupRequestDto;
 import com.project.delivery.entity.User;
+import com.project.delivery.exception.ProfileNotFoundExistException;
+import com.project.delivery.exception.UsernameAlreadyExistException;
 import com.project.delivery.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -15,10 +17,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -47,7 +49,6 @@ class UserServiceTest {
             given(dto.username()).willReturn("username");
             given(userRepository.existsByUsername(anyString())).willReturn(false);
             given(dto.password()).willReturn("password");
-            given(userService.existsUsername(anyString())).willReturn(false);
             given(passwordEncoder.encode(any(CharSequence.class))).willReturn("encodedPassword");
 
             //  when
@@ -57,7 +58,6 @@ class UserServiceTest {
             verify(userRepository).existsByUsername(anyString());
             verify(userRepository).save(userCaptor.capture());
             assertThat(dto.password()).isNotEqualTo(userCaptor.getValue().getPassword());
-
         }
 
         @DisplayName("existsUsername(): 유저가 존재할 시 true 반환")
@@ -104,5 +104,36 @@ class UserServiceTest {
         }
     }
 
+    @DisplayName("실패 케이스")
+    @Nested
+    class Fail {
+        @DisplayName("중복된 Username 으로 회원가입시 UsernameAlreadyExistException 반환")
+        @Test
+        void signup_throw_UsernameAlreadyExistException() {
+            //  given
+            SignupRequestDto dto = mock(SignupRequestDto.class);
+            given(dto.username()).willReturn("username");
+            given(userRepository.existsByUsername(anyString())).willReturn(true);
+            given(userService.existsUsername(anyString())).willReturn(true);
 
+            //  when
+            assertThatThrownBy(() -> userService.signup(dto))
+                    .isInstanceOf(UsernameAlreadyExistException.class);
+
+            //  then
+            verify(userRepository).existsByUsername(anyString());
+        }
+
+        @DisplayName("{id}값과 일치하는 유저 정보가 없을 시 ProfileNotFoundException 반환")
+        @Test
+        void myProfile_throw_ProfileNotFoundException() {
+            //  given
+            given(userRepository.findById(anyLong())).willReturn(Optional.empty());
+            //  when
+            assertThatThrownBy(() -> userService.myProfile(1L))
+                    .isInstanceOf(ProfileNotFoundExistException.class);
+            //  then
+            verify(userRepository).findById(anyLong());
+        }
+    }
 }
